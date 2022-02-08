c8424f235b3af7fa4fe646e497be7f8edce1015d
İSRAFİL HALLAÇ <83336393+IsrafilHallac-ih@users.noreply.github.com>
2022-02-08 08:59:50 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,18 @@
+x,y,z=5,15,20
+
+x +=5   # x= x+5
+x -=5   # x= x-5
+x *=5   # x= x*5 
+x /=5   # x= x/5
+x %=5   # x= x%5 mod alma
+y //=5  # y= y//5 tam bölme
+y **=5  # y= y**5 üs alma
+
+# print(x,y,z)
+values=1,2,3,4,5
+print(values)
+print(type(values))
+x,y,*z=values
+print(x,y,z)
+print(x,y,z[2])
+
